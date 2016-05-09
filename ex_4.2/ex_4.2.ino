99b502e723409ba9b8c06475c8a43cc4f2c4d2f4
rogerlconflow <rogerlladom@gmail.com>
2016-05-09 16:19:12 +0200

    4.2
    variacio melodia amb 2 pot.

---
+++ ex_4.2/ex_4.2.ino
@@ -0,0 +1,22 @@
+const int xiulet = 9;
+const int pot0 = A0;
+const int pot1 = A1; 
+int valPot0; 
+int valPot1;
+
+
+void setup()
+{
+ pinMode(xiulet,OUTPUT);
+ // definim com a sortida 
+}
+
+void loop() 
+{
+ valPot0 = analogRead(pot0);
+ valPot1 = analogRead(pot1);
+
+tone(xiulet, valPot1, valPot0); 
+delay(2*valPot0);    // put your main code here, to run repeatedly:
+
+}
